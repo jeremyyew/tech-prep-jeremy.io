4ab36a88db6ce43a728cd2e2c33ff3ad963d38d6
jeremyyew <yewern@gmail.com>
2019-05-18 11:45:07 +0800

    Add combination sum

---
+++ code/5-dynamic-programming/E377-combination-sum-IV.py
@@ -0,0 +1,20 @@
+'''
+We can obtain the number of combinations that form a target i by checking how many combinations form each sub-target k, where k + n = i and n is a number we add to combinations of k to form i.  
+
+More formally: 
+    dp[i] = sum{dp[i-n] where i > n, and if i == n then 1}
+
+Time complexity: O(target * |nums|). 
+'''
+
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        if target < 1:
+            return 0
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        for i in range(1, target + 1):
+            for n in nums:
+                dp[i] += dp[i-n] if i >= n else 0
+        return dp[target]
