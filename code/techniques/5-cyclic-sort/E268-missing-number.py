b05841bfb4a8529f2a551d3942efff424914aa7c
jeremyyew <yewern@gmail.com>
2019-05-29 11:42:50 +0800

    Update E448

--- code/techniques/5-cyclic-sort/E268-missing-number.py
+++ code/techniques/5-cyclic-sort/E268-missing-number.py
@@ -3,20 +3,23 @@
     Collect in array, scan for missing value.
     We can use Counter to elegantly collect all.
 
-2. **Collect with bitmask**: O(N) time, O(N) space but in bits.
+2. **Collect with set**: O(N) time, O(N) space.
+    Even more elegant than Counter.
+
+3. **Collect with bitmask**: O(N) time, O(N) space but in bits.
     Assuming we usually use an int or bool for each number (as in Solution 0), those are 24 bytes = 192 bits each in Python. Alternatively we could use a bitmask. Theoretically, we use exactly N bits. So it's O(N/192) space. In practice a binary number in python is min size 34, LOL.
 
-3. **Cyclic Sort**: O(N) time, O(1) space.
+4. **Cyclic Sort**: O(N) time, O(1) space. (Submitted)
     1. Outer loop over `i`.
     2. Inner loop to jump around correcting displacements until we reach a non-displacement, then continue outer loop.
     3. Remember to check for index out-of-bounds at every step; as any number could be `n`.  
     4. Run through again to find missing.  
     4. If there were no missing numbers, then `n` itself is the missing number.  
 
-4. **Gaussian Sum**: O(N) time, O(1) space. Simple, elegant.  
+5. **Gaussian Sum**: O(N) time, O(1) space. Simple, elegant.  
     See below.
 
-5. **XOR**: O(N) time, O(1) space. Elegant.
+6. **XOR**: O(N) time, O(1) space. Elegant.
     - We make use of the fact that `XOR` is commutative to cancel out all values except the missing one i.e. `y XOR y` = 0 and `y XOR y XOR k = k`.
     - The indices of the arrays represent the numbers that should be there.
     - If we 'cancel out' pairs of indices and values that are both present, then we will be left with the indice `k` that is missing the value `k`, as well as the extra value `n` that shouldn't be there.
@@ -25,7 +28,7 @@
     [1] Start with `n`.  
     [2] For each index `i`, `n = n XOR i XOR nums[i]`.  
 
-6. **Binary search**: O(NlogN) time, O(1) space. (Submitted)  
+7. **Binary search**: O(NlogN) time, O(1) space.
     [1] Sort.  
     [2] Binary search to see if value is displaced from corresponding index; find the start of displacement.  
     [3] If `m` displaced, missing on left of `m`, inclusive of `m`.  
@@ -49,6 +52,11 @@ class SolutionCollectCounter:
         return missing[0]
 
 
+class SolutionCollectSet:
+    def missingNumber(self, nums):
+        return list(set(range(len(nums) + 1)) - set(nums))[0]
+
+
 class SolutionCollectBitMask:
     def missingNumber(self, nums):
         def positionOfSetBit(n):
@@ -65,7 +73,7 @@ class SolutionCollectBitMask:
         return missing
 
 
-class SolutionCyclicSort:
+class Solution:
     def missingNumber(self, nums):
         i, n = 0, len(nums)
         for i in range(n):  # [1]
@@ -97,7 +105,7 @@ class SolutionXOR:
         return n
 
 
-class Solution:
+class SolutionBinarySearch:
     def missingNumber(self, nums):
         nums.sort()  # [1]
         l, r = 0, len(nums)
