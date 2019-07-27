6a597acc7a9b825f36bf2f57abc6b21ce9efd5a7
jeremyyew <yewern@gmail.com>
2019-07-27 23:42:00 +0800

    Add E20, Add H301

---
+++ code/topics/9-miscellaneous/E20-valid-parentheses.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk, mapping = [], {'(': ')', '[': ']', '{': '}'},
+        for c in s:
+            if c in ('(', '{', '['):
+                stk.append(c)
+            else:
+                if not stk or mapping[stk.pop()] != c:
+                    return False
+        return (not stk)
