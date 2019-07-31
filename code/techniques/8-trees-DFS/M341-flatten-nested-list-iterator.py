7bb6dad5c397ebf0dc2fea81a5b17541064125c4
Jeremy Yew <jeremy.yew@sap.com>
2019-07-31 15:11:24 +0800

    Update E26, E27, M341

--- code/techniques/8-trees-DFS/M341-flatten-nested-list-iterator.py
+++ code/techniques/8-trees-DFS/M341-flatten-nested-list-iterator.py
@@ -1,3 +1,9 @@
+'''
+- Only pop and unpack what is necessary. 
+- Pop and unpack when `hasNext` is called - it ensures there is a next available for `next`, if there really is a next. 
+- At the end only need to check if stack is nonempty - stack nonempty and last element not integer is not possible.
+'''
+
 class NestedIterator(object):
 
 	def __init__(self, nestedList):
