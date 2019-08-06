bcc9565a29784e3b2cfcc21dae2a88a6a715b5b7
Jeremy Yew <jeremy.yew@sap.com>
2019-08-06 16:53:41 +0800

    Add M199

---
+++ code/techniques/8-DFS/M199-binary-tree-right-side-view.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rightSideView(self, root):
+        if not root: 
+            return []
+        view, stk = [], [(root, 0)]
+        while stk: 
+            node, depth = stk.pop()
+            if depth + 1 > len(view):
+                view.append(node.val)
+            else: 
+                view[depth] = node.val 
+            if node.right:
+                stk.append((node.right, depth + 1))
+            if node.left: 
+                stk.append((node.left, depth + 1))
+        
+        return view
