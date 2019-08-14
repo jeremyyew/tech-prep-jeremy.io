1b87685685b14e02b423f508e2354d409cd32fc4
jeremyyew <yewern@gmail.com>
2019-08-14 15:33:26 +0800

    Add M347, M647

---
+++ code/topics/9-miscellaneous/M647-palindromic-substrings.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def countSubstrings(self, S):
+        N = len(S)
+        ans = 0
+        for i in range(2*N - 1):
+            l = i // 2
+            r = l + (i // 2)
+            while l >= 0 and r < N and S[l] == S[r]:
+                ans += 1
+                l -= 1
+                r += 1
+        return ans
