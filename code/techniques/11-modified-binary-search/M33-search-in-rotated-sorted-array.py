782041e5a4d8a83b868e4af663833e62ce31d9ec
jeremyyew <yewern@gmail.com>
2019-10-23 23:15:41 +0800

    Refactor H329

--- code/techniques/11-modified-binary-search/M33-search-in-rotated-sorted-array.py
+++ code/techniques/11-modified-binary-search/M33-search-in-rotated-sorted-array.py
@@ -1,34 +1,26 @@
 '''
-The unexpectedly simple trick to make binary search work despite the rotation: essentially, using an offset to get the true midpoint - otherwise the binary search is exactly the same! 
+https://leetcode.com/problems/search-in-rotated-sorted-array/
 
-[1] We project from the current midpoint index to get the 'real' midpoint value (via a modulus shift of the rotation places), which we then compare with the target. 
+The unexpectedly simple trick to make binary search work despite the rotation: essentially, using an offset to get the true midpoint - otherwise the binary search is exactly the same!
 
-[2] Consequently, moving l and r as per normal binary search will also shift the realMid along the ordered array. 
+[1] We project from the current midpoint index to get the 'real' midpoint value (via a modulus shift of the rotation places), which we then compare with the target.
 
-- For example, if nums[realMid] < target, we move l to the mid. Yes we might have left the target outside of nums[l:r]; however, we are trying to zero in on the target using our offset realMid, not mid directly, so that's ok. Thus we are actually shifting realMid to make our next guess to the right by (m-l)//2 places.   
+[2] Consequently, moving l and r as per normal binary search will also shift the realMid along the ordered array.
 
-[3] Of course, we also have to obtain the rotation point; see M153-find-minimum-in-rotated-sorted-array for explanation. 
+- For example, if nums[realMid] < target, we move l to the mid. Yes we might have left the target outside of nums[l:r]; however, we are trying to zero in on the target using our offset realMid, not mid directly, so that's ok. Thus we are actually shifting realMid to make our next guess to the right by (m-l)//2 places.
+
+[3] Of course, we also have to obtain the rotation point; see M153-find-minimum-in-rotated-sorted-array for explanation.
 
 [4] Also remember to make findMin return index not value!
 
-Adapted from https://leetcode.com/problems/search-in-rotated-sorted-array/discuss/14425/Concise-O(log-N)-Binary-search-solution. 
+Adapted from https://leetcode.com/problems/search-in-rotated-sorted-array/discuss/14425/Concise-O(log-N)-Binary-search-solution.
 '''
+from typing import List
 
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        rotation = self.findMin(nums)  # [3], [4]
-        l, r = 0, len(nums)-1
-        while l <= r:  # [2]
-            m = (l+r)//2
-            realMid = (m + rotation) % len(nums)  # [1]
-            if nums[realMid] == target:
-                return realMid
-            if nums[realMid] < target:
-                l = m+1
-            else:
-                r = m-1
-        return -1
+        pass
 
     def findMin(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
@@ -43,3 +35,35 @@ class Solution:
             else:
                 r = m
         return l
+
+        # class Solution:
+        #     def search(self, nums: List[int], target: int) -> int:
+        #         rotation = self.findMin(nums)  # [3], [4]
+        #         l, r = 0, len(nums)-1
+        #         while l <= r:  # [2]
+        #             m = (l+r)//2
+        #             realMid = (m + rotation) % len(nums)  # [1]
+        #             if nums[realMid] == target:
+        #                 return realMid
+        #             if nums[realMid] < target:
+        #                 l = m+1
+        #             else:
+        #                 r = m-1
+        #         return -1
+
+        #     def findMin(self, nums: List[int]) -> int:
+        #         l, r = 0, len(nums) - 1
+        #         if len(nums) == 0:
+        #             return None
+        #         if nums[r] > nums[l]:
+        #             return l
+        #         while l < r:
+        #             m = (l + r) // 2
+        #             if nums[m] > nums[r]:
+        #                 l = m + 1
+        #             else:
+        #                 r = m
+        #         return l
+
+
+assert(Solution().search([4, 5, 6, 7, 0, 1, 2], 0) == 4)
--- code/techniques/15-topological-sort/H329-longest-increasing-path-in-a-matrix.py
+++ code/techniques/15-topological-sort/H329-longest-increasing-path-in-a-matrix.py
@@ -1,7 +1,7 @@
 '''
-- Memoize max path distance starting from every cell. Exactly like grid traversal DP, we must simply obtain all max paths. 
-- Instead of re-traversing a cell that has already been computed, we lookup first in the memo. This keeps time to O(N), otherwise it could be O(N^2) or more. 
-- We may only register and return the max path for the current cell once we have obtained the results from all its legal adjacent cells. 
+- Memoize max path distance starting from every cell. Exactly like grid traversal DP, we must simply obtain all max paths.
+- Instead of re-traversing a cell that has already been computed, we lookup first in the memo. This keeps time to O(N), otherwise it could be O(N^2) or more.
+- We may only register and return the max path for the current cell once we have obtained the results from all its legal adjacent cells.
 '''
 
 from typing import List
@@ -9,38 +9,31 @@ from typing import List
 
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        if len(matrix) == 0:
+        if not matrix:
             return 0
-        nrows = len(matrix)
-        ncols = len(matrix[0])
-        maxLengths = [[None] * ncols for _ in range(nrows)]
+        I, J = len(matrix), len(matrix[0])
+        dp = [[None for _ in range(J)] for _ in range(I)]
 
-        def legalSteps(x, y):
-            steps = []
-            curr = matrix[x][y]
-            if x > 0 and matrix[x-1][y] > curr:
-                steps.append((x-1, y))
-            if x < nrows - 1 and matrix[x+1][y] > curr:
-                steps.append((x+1, y))
-            if y > 0 and matrix[x][y-1] > curr:
-                steps.append((x, y-1))
-            if y < ncols - 1 and matrix[x][y+1] > curr:
-                steps.append((x, y+1))
-            return steps
+        def neighbors(i, j):
+            nbs = [(i-1, j), (i+1, j),
+                   (i, j-1), (i, j+1)]
+            return [(i, j) for (i, j) in nbs
+                    if 0 <= i < I
+                    and 0 <= j < J]
 
-        def maxLengthFrom(x, y):
-            if maxLengths[x][y]:
-                return maxLengths[x][y]
-            res = max([maxLengthFrom(p, q)
-                       for p, q in legalSteps(x, y)] + [0]) + 1
-            maxLengths[x][y] = res
+        def max_path_from(i, j):
+            if dp[i][j]:
+                return dp[i][j]
+            increasing_nbs = [(x, y) for x, y in neighbors(i, j)
+                              if matrix[x][y] > matrix[i][j]]
+            res = max([max_path_from(x, y)
+                       for x, y in increasing_nbs], default=0) + 1
+            dp[i][j] = res
             return res
 
-        lengths = []
-        for x in range(nrows):
-            for y in range(ncols):
-                lengths.append(maxLengthFrom(x, y))
-        return max(lengths)
+        path_lengths = [max_path_from(i, j)
+                        for j in range(J) for i in range(I)]
+        return max(path_lengths)
 
 
 # r = Solution().longestIncreasingPath(
@@ -50,3 +43,14 @@ class Solution:
 #         [2, 2, 1]
 #     ])
 # print(r)
+# assert(r == 4)
+
+
+# r = Solution().longestIncreasingPath(
+#     [
+#         [9, 9, 4],
+#         [6, 6, 8],
+#         [2, 1, 1]
+#     ])
+# print(r)
+# assert(r == 4)
